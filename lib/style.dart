b2c96226da2636f062a175110a17d0e263cefa4c
이지윤 <02leejiyun@naver.com>
2023-11-09 02:48:40 +0900

    [FEAT] 색상 코드 추가

--- lib/style.dart
+++ lib/style.dart
@@ -4,6 +4,10 @@ class AppColor extends Color {
   AppColor(super.value);
 
   static const Blue = Color(0xFF3267E3);
+  static const Blue1 = Color(0xFFF0F3FF);
+  static const Blue2 = Color(0xFFB1C5F6);
+  static const Blue3 = Color(0xFF114CD6);
+  static const Blue4 = Color(0xFF11317D);
 
   static const Grey = Color(0xFF616161);
   static const Grey1 = Color(0xFFC2C2C2);
