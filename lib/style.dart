e114454a7685ece4570ab03bf0b1ce4a47ba098e
이지윤 <02leejiyun@naver.com>
2023-11-09 02:47:48 +0900

    [ADD] style.dart 파일 추가

---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor extends Color {
+  AppColor(super.value);
+
+  static const Blue = Color(0xFF3267E3);
+
+  static const Grey = Color(0xFF616161);
+  static const Grey1 = Color(0xFFC2C2C2);
+}
