3e77ef0c43be5bde49b180b71f59f67b6dea7d7f
이지윤 <02leejiyun@naver.com>
2023-11-12 15:16:34 +0900

    [STYLE] 앱 색상 변경

--- lib/style.dart
+++ lib/style.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class AppColor extends Color {
   AppColor(super.value);
 
-  static const Blue = Color(0xFF3267E3);
-  static const Blue1 = Color(0xFFF0F3FF);
-  static const Blue2 = Color(0xFFB1C5F6);
-  static const Blue3 = Color(0xFF114CD6);
-  static const Blue4 = Color(0xFF11317D);
+  static const Blue = Color(0xFF1864AB);
+  static const Blue1 = Color(0xFFF3F9FF);
+  static const Blue2 = Color(0xFFA5D3F6);
+  static const Blue3 = Color(0xFF339AF0);
+  static const Blue4 = Color(0xFF0D365D);
 
   static const Grey = Color(0xFF616161);
   static const Grey1 = Color(0xFFC2C2C2);
