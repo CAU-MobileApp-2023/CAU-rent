e0b3b210f1c36dbe2ebc9e5d29082efc5128a1cd
이지윤 <02leejiyun@naver.com>
2023-11-29 04:12:57 +0900

    [FEAT] 로그인 validator 추가

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,9 +11,9 @@ class LogInPage extends StatefulWidget {
 }
 
 class _LogInPageState extends State<LogInPage> {
-  Map<String, Map<String, String>> users = {};
+  Map<String, Map<String, String>> users = {};      // data
 
-  TextEditingController idController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool isLoggedIn = false;
 
@@ -27,138 +27,179 @@ class _LogInPageState extends State<LogInPage> {
   }
 
   Widget LogInScreen() {
+    final _formKey = GlobalKey<FormState>();
+
     return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Image.asset(
-              'assets/CAU_rent.png',
-              height: 120,
+      child: Form(
+      key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                'assets/CAU_rent.png',
+                height: 120,
+              ),
             ),
-          ),
 
-          /*
-          const Text(
-            'CAU rent',
-            style: TextStyle(fontSize: 28, color: AppColor.Blue, fontWeight: FontWeight.bold),
-          ),
-           */
-
-
-          /* ----------------------------------- */
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-            child: const Text(
+            /*
+            const Text(
               'CAU rent',
               style: TextStyle(fontSize: 28, color: AppColor.Blue, fontWeight: FontWeight.bold),
             ),
-          ),
-          /* ----------------------------------- */
+             */
 
+            /* ----------------------------------- */
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: const Text(
+                'CAU rent',
+                style: TextStyle(fontSize: 28, color: AppColor.Blue, fontWeight: FontWeight.bold),
+              ),
+            ),
+            /* ----------------------------------- */
+            const SizedBox(height: 20),
 
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 280,
-            child: TextFormField(
-              controller: idController,
-              decoration: const InputDecoration(
-                labelText: 'ID',
-                hintText: '아이디를 입력하세요',
-                labelStyle: TextStyle(
-                  color: AppColor.Blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 2,
+            SizedBox(
+              width: 280,
+              child: TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: '이메일을 입력하세요',
+                  labelStyle: TextStyle(
                     color: AppColor.Blue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: AppColor.Blue,
+                    ),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.red,
+                    ),
                   ),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter email.';
+                  }
+                  return null;
+                },
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          SizedBox(
-            width: 280,
-            child: TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: '비밀번호를 입력하세요',
-                labelStyle: TextStyle(
-                  color: AppColor.Blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 2,
+
+            const SizedBox(height: 16),
+
+            SizedBox(
+              width: 280,
+              child: TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: '비밀번호를 입력하세요',
+                  labelStyle: TextStyle(
                     color: AppColor.Blue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: AppColor.Blue,
+                    ),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.red,
+                    ),
                   ),
                 ),
-              ),
-              obscureText: true,
-            ),
-          ),
-          const SizedBox(height: 60),
-          ElevatedButton(
-            onPressed: () => _performLogin(),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              fixedSize: MaterialStateProperty.all<Size>(const Size(280, 50)),
-              shape: MaterialStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password.';
+                  }
+                  return null;
+                },
               ),
             ),
-            child: const Text(
-              'Log In',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          const SizedBox(height: 18),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpPage(users)),
-              );
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              foregroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
-              side: MaterialStateProperty.all<BorderSide>(const BorderSide(color: AppColor.Blue)),
-              fixedSize: MaterialStateProperty.all<Size>(const Size(280, 50)),
-              shape: MaterialStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
+
+            const SizedBox(height: 60),
+
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // 폼 검증 성공 시 로그인 수행
+                  _performLogin();
+                }
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                fixedSize: MaterialStateProperty.all<Size>(const Size(280, 50)),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )
+                ),
+              ),
+              child: const Text(
+                'Log In',
+                style: TextStyle(fontSize: 20),
               ),
             ),
-            child: const Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 20),
+
+            const SizedBox(height: 18),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage(users)),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                foregroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
+                side: MaterialStateProperty.all<BorderSide>(const BorderSide(color: AppColor.Blue)),
+                fixedSize: MaterialStateProperty.all<Size>(const Size(280, 50)),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )
+                ),
+              ),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-        ],
+
+            const SizedBox(height: 10),
+
+          ],
+        ),
       ),
     );
+
   }
 
   void _performLogin() {
-    String enteredId = idController.text;
+    String enteredId = emailController.text;
     String enteredPassword = passwordController.text;
 
     if (enteredId.isNotEmpty && enteredPassword.isNotEmpty) {
@@ -170,10 +211,6 @@ class _LogInPageState extends State<LogInPage> {
       } else {
         _showSnackBar('아이디 또는 비밀번호가 잘못되었습니다', Colors.red);
       }
-    } else if (enteredId.isEmpty) {
-      _showSnackBar('아이디를 입력하세요', Colors.red);
-    } else if (enteredPassword.isEmpty) {
-      _showSnackBar('비밀번호를 입력하세요', Colors.red);
     }
   }
 
