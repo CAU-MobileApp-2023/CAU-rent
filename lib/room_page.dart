d854ea793bf69a874f9c436bb85c76a18a5dfe63
yejin923 <yejin923@naver.com>
2023-12-03 16:40:06 +0900

    modified reservation time

--- lib/room_page.dart
+++ lib/room_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:teamproject/style.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/cupertino.dart';
 
 
 class RoomPage extends StatefulWidget {
@@ -11,23 +12,113 @@ class RoomPage extends StatefulWidget {
 }
 
 class _RoomPageState extends State<RoomPage> {
-  late Map<int, bool> projectRoomRentalStatus;
-  final projectRooms = List.generate(5, (i) => i + 1);
+  //late Map<int, bool> projectRoomRentalStatus;
+  //final projectRooms = List.generate(5, (i) => i + 1);
 
-  TimeOfDay startTime = TimeOfDay (hour:0,minute: 0); // 예약 시작 시간
-  TimeOfDay endTime = TimeOfDay (hour:0,minute: 0); // 예약 마감 시간
+  final _rooms =['--선택해 주세요--','팀프로젝트실1 (208관 601호)','팀프로젝트실2 (208관 601호)','팀프로젝트실3 (208관 601호)','팀프로젝트실4 (208관 601호)','팀프로젝트실5 (208관 601호)'];
+  String? _selectedRoom;
 
   @override
   void initState() {
     super.initState();
+    setState(() {
+      _selectedRoom=_rooms[0];
+    });
     // 각 팀플룸 대여 상태 저장
-    projectRoomRentalStatus = { for (var item in projectRooms) item : item % 2 == 0 };
+    // projectRoomRentalStatus = { for (var item in projectRooms) item : item % 2 == 0 };
   }
 
-  String getTomorrow(){ // 예약 날짜(내일)
+  String getTomorrow(){ // 예약 날짜(내일) 받아오는 함수
     DateTime tomorrow = DateTime.now().add(Duration(days:1));
     DateFormat formatter = DateFormat('yyyy-MM-dd');
     return formatter.format(tomorrow);
+  }
+
+  void _showTimeDialog(BuildContext context) { // 예약 시간 팝업창
+    final List<int> _items = List.generate(5, (index) => index*2 + 9);
+    int result = _items[0];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Text(getTomorrow()),
+          content: Container(
+            height: 180,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: CupertinoPicker(
+                    itemExtent: 40.0,
+                    onSelectedItemChanged: (int index) {
+                      result = _items[index];
+                    },
+                    children: _items
+                        .map((e) => Text(
+                      '$e:00 - ${e+2}:00',
+                      textAlign: TextAlign.center,
+                    ))
+                        .toList(),
+                  ),
+                ),
+                SizedBox(height: 30,),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // AlertDialog 닫기
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all<Color>(AppColor.Blue),
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                            const Size(100, 40)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Confirm',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // AlertDialog 닫기
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        foregroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
+                        side: MaterialStateProperty.all<BorderSide>(const BorderSide(color: AppColor.Blue, width: 2.0)),
+                        fixedSize: MaterialStateProperty.all<Size>(const Size(100, 40)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0), // 버튼의 모서리를 둥글게
+                            )
+                        ),
+                      ),
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -51,122 +142,84 @@ class _RoomPageState extends State<RoomPage> {
                     Tab(text: 'Team Project Room (208관 601호)'),
                   ]
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 40),
 
-              Text(getTomorrow(),style: const TextStyle(fontSize:24,fontWeight: FontWeight.bold),),
-
-              const SizedBox(
-                height: 5,
-              ),
-
-              Text('${startTime.hour}:${startTime.minute} - ${endTime.hour}:${endTime.minute}',style: const TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
-
-              const SizedBox(height: 10),
-
-              ElevatedButton(onPressed: () async { // 예약 시간 설정
-               final TimeOfDay? timeOfDay1 = await showTimePicker(context: context, initialTime: startTime);
-               final TimeOfDay? timeOfDay2 = await showTimePicker(context: context, initialTime: endTime);
-
-                if(timeOfDay1 != null || timeOfDay2 != null){
-                  if(timeOfDay2!.hour-timeOfDay1!.hour>2 || timeOfDay2.hour-timeOfDay1!.hour<0){ // 예약 시간 설정 오류 시 팝업창
-                      _showInvalidTimeIntervalDialog();
-                  }
-                  else{
-                    setState(() {
-                      startTime = timeOfDay1!;
-                      endTime = timeOfDay2!;
-                    });
-                    }
-                }
-              },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColor.Blue4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      )
-                    ),
-                  child: Text('예약 시간')
-              ),
-
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: projectRooms.map((i) => TextButton.icon(
-                    onPressed: () {
-                      if (projectRoomRentalStatus[i] == true) {
-                        _showUnavailableDialog(context, i);
-                      } else {
-                        _showAvailableDialog(context, i);
-                      }
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.forum_rounded, color: AppColor.Blue4, size: 40),
+                  const SizedBox( width: 20),
+                  DropdownButton(
+                    value: _selectedRoom,
+                    items: _rooms
+                        .map((e)=> DropdownMenuItem(value:e, child: Text(e))).toList(),
+                    onChanged: (value){
+                      setState(() {
+                        _selectedRoom = value!;
+                      });
                     },
-                    icon: projectRoomRentalStatus[i] == true
-                        ? const Icon(Icons.forum_rounded, color: AppColor.Grey1, size: 34)
-                        : const Icon(Icons.forum_rounded, color: AppColor.Blue4, size: 34),
-                    label: Text(
-                        "  Team Project Room #$i",
-                        style: projectRoomRentalStatus[i] == true
-                             ? const TextStyle(fontSize: 24, color: AppColor.Grey1, fontWeight: FontWeight.bold)
-                             : const TextStyle(fontSize: 24, color: AppColor.Blue4, fontWeight: FontWeight.bold)
-                    ),
-                  )).toList(),
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
+
+             SizedBox(height: 30,),
+
+             SingleChildScrollView(
+                scrollDirection:Axis.horizontal,
+                child: SingleChildScrollView(
+                 child: Container(
+                     margin: EdgeInsets.all(16.0),
+                     decoration: BoxDecoration(
+                       border: Border.all(
+                         width: 1,
+                         color: AppColor.Grey1,
+                       ),
+                     ),
+
+                     child: DataTable(columns: const [
+                       DataColumn(label: Text('날짜')),
+                       DataColumn(label: Text('시간'), numeric: true),
+                       DataColumn(label: Text('예약자명')),
+                       //DataColumn(label: Text('인원수')),
+                       //DataColumn(label: Text('이용목적')),
+                     ], rows:  [                   // 예약 현황 표 예시
+                       DataRow(cells: [
+                         DataCell(Text(getTomorrow())),
+                         DataCell(Text('09:00-11:00')),
+                         DataCell(Text('김푸앙')),
+                         //DataCell(Text('5')),
+                         //DataCell(Text('팀플')),
+                       ]),
+                       DataRow(cells: [
+                         DataCell(Text(getTomorrow())),
+                         DataCell(Text('13:00-15:00')),
+                         DataCell(Text('이푸앙')),
+                         //DataCell(Text('3')),
+                         //DataCell(Text('캡스톤')),
+                       ]),
+                     ]),
+                 ),
+               ),
+             ),
+
+             const SizedBox(height: 20,),
+
+             ElevatedButton(onPressed: () {_showTimeDialog(context);},
+                 style: ElevatedButton.styleFrom(
+                 backgroundColor: AppColor.Blue,
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(15.0),
+                 )
+             ),
+                 child: const Text('Reserve',style: TextStyle(fontSize:20),),
+             ),
             ],
           ),
         )
     );
   }
 
-  void _showInvalidTimeIntervalDialog() { // 예약 시간 오류 팝업
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text('예약 가능 시간은 최대 2시간 입니다.'),
-              const SizedBox( height: 10,),
-              const Text(
-                'You can make a reservation 2 hours at most',
-                style: TextStyle(fontSize: 14.4),
-              ),
-              const SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(); // AlertDialog 닫기
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(AppColor.Blue),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      fixedSize: MaterialStateProperty.all<Size>(const Size(140, 50)),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0), // 버튼의 모서리를 둥글게
-                          )
-                      ),
-                    ),
-                    child: const Text(
-                      'Confirm',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
+  /* 이 아래는 예약 가능 여부 팝업창인데 나중에 백엔드랑 연동 후에 필요할 것 같아서 그대로 둘게요!*/
 
   void _showAvailableDialog(BuildContext context, int equipmentNum) {
     showDialog(
