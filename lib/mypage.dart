5f918318cd17c2a5450dfbfcd859f34f47cf9a8c
이지윤 <02leejiyun@naver.com>
2023-12-05 17:50:14 +0900

    [FIX] 마이페이지 데이터 연동 오류 수정

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -13,60 +13,57 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  List<bool> rentalStatus = [false, false, false];    // MacBook, LG gram, WebCam
+  List<bool> equipRentalStatus = [false, false, false];    // MacBook, LG gram, WebCam
+  String macBookDueDate = '-';
+  String lgGramDueDate = '-';
+  String webCamDueDate = '-';
 
-  Future<String> getMacBookDuedate(String studentId) async {
-    var result = await http.get(
+  Future<void> _setEquipRentalStatus(String studentId) async {
+    http.Response result;
+    result = await http.get(
         Uri.parse('http://10.0.2.2:8000/rental_records/devices/$studentId/now/')
     );
-    String dueDate = '-';
     if (result.statusCode == 200) {
       List<dynamic> responseData = jsonDecode(result.body);
       for (var data in responseData) {
         int deviceId = int.parse(data['device'].toString());
         if (deviceId <= 27) {
-          dueDate = data['end_date'].toString().substring(0, 10);
-          rentalStatus[0] = true;
+          macBookDueDate = data['end_date'].toString().substring(0, 10);
+          equipRentalStatus[0] = true;
         }
       }
     }
-    return dueDate;
-  }
 
-  Future<String> getLgGramDuedate(String studentId) async {
-    var result = await http.get(
+
+    result = await http.get(
         Uri.parse('http://10.0.2.2:8000/rental_records/devices/$studentId/now/')
     );
-    String dueDate = '-';
     if (result.statusCode == 200) {
       List<dynamic> responseData = jsonDecode(result.body);
       for (var data in responseData) {
         int deviceId = int.parse(data['device'].toString());
         if (27 < deviceId && deviceId <= 27 + 22) {
-          dueDate = data['end_date'].toString().substring(0, 10);
-          rentalStatus[1] = true;
+          lgGramDueDate = data['end_date'].toString().substring(0, 10);
+          equipRentalStatus[1] = true;
         }
       }
     }
-    return dueDate;
-  }
 
-  Future<String> getWebCamDuedate(String studentId) async {
-    var result = await http.get(
+
+    result = await http.get(
         Uri.parse('http://10.0.2.2:8000/rental_records/devices/$studentId/now/')
     );
-    String dueDate = '-';
     if (result.statusCode == 200) {
       List<dynamic> responseData = jsonDecode(result.body);
       for (var data in responseData) {
         int deviceId = int.parse(data['device'].toString());
         if (deviceId > 27 + 22) {
-          dueDate = data['end_date'].toString().substring(0, 10);
-          rentalStatus[2] = true;
+          webCamDueDate = data['end_date'].toString().substring(0, 10);
+          equipRentalStatus[2] = true;
         }
       }
     }
-    return dueDate;
+
   }
 
   @override
@@ -159,118 +156,105 @@ class _MyPageState extends State<MyPage> {
               const SizedBox(height: 25),
 
 
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    SizedBox(
-                      width: 100,
-                      child: Column(
-                          children:[
-                            const Text('MacBook', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
-                            const SizedBox(height: 8),
-                            Container(
-                              height: 2.0,
-                              width: 70.0,
-                              color: AppColor.Blue,
+
+
+
+              FutureBuilder<void>(
+                future: _setEquipRentalStatus(context.read<UserProvider>().userStudentId),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState != ConnectionState.done) {
+                    return const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // SizedBox(height: 180),
+                        CircularProgressIndicator()
+                      ],
+                    );
+                  } else {
+                     return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          SizedBox(
+                            width: 100,
+                            child: Column(
+                                children:[
+                                  const Text('MacBook', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
+                                  const SizedBox(height: 8),
+                                  Container(
+                                    height: 2.0,
+                                    width: 70.0,
+                                    color: AppColor.Blue,
+                                  ),
+                                  const SizedBox(height: 14),
+
+                                  equipRentalStatus[0]
+                                      ? const Icon(Icons.laptop_mac, color: AppColor.Blue4, size: 65)
+                                      : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
+
+
+                                  Text(macBookDueDate, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+
+                                ]
                             ),
-                            const SizedBox(height: 14),
+                          ),
 
-                            rentalStatus[0]
-                                ? const Icon(Icons.laptop_mac, color: AppColor.Blue4, size: 65)
-                                : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
+                          SizedBox(
+                            width: 100,
+                            child: Column(
+                                children:[
+                                  const Text('LG gram', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
+                                  const SizedBox(height: 8),
+                                  Container(
+                                    height: 2.0,
+                                    width: 70.0,
+                                    color: AppColor.Blue,
+                                  ),
+                                  const SizedBox(height: 14),
 
+                                  equipRentalStatus[1]
+                                      ? const Icon(Icons.laptop_windows, color: AppColor.Blue4, size: 65)
+                                      : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
 
-                            FutureBuilder<String>(
-                              future: getMacBookDuedate(context.read<UserProvider>().userStudentId),
-                              builder: (context, snapshot) {
+                                  Text(lgGramDueDate, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
 
-                                if (snapshot.hasData) {
-                                  return Text(
-                                    snapshot.data.toString(),
-                                    style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-                                  );
-                                }
-                                return const Text('-', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15));
-                              },
+                                ]
                             ),
+                          ),
 
 
+                          SizedBox(
+                            width: 100,
+                            child: Column(
+                                children:[
+                                  const Text('WebCam', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
+                                  const SizedBox(height: 8),
+                                  Container(
+                                    height: 2.0,
+                                    width: 70.0,
+                                    color: AppColor.Blue,
+                                  ),
+                                  const SizedBox(height: 14),
 
-                          ]
-                      ),
-                    ),
+                                  equipRentalStatus[2]
+                                      ? const Icon(Icons.photo_camera, color: AppColor.Blue4, size: 65)
+                                      : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
 
-                    SizedBox(
-                      width: 100,
-                      child: Column(
-                          children:[
-                            const Text('LG gram', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
-                            const SizedBox(height: 8),
-                            Container(
-                              height: 2.0,
-                              width: 70.0,
-                              color: AppColor.Blue,
+
+                                  Text(webCamDueDate, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+
+                                ]
                             ),
-                            const SizedBox(height: 14),
+                          ),
+                        ]
 
-                            rentalStatus[1]
-                              ? const Icon(Icons.laptop_windows, color: AppColor.Blue4, size: 65)
-                              : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
-
-
-                            FutureBuilder<String>(
-                              future: getLgGramDuedate(context.read<UserProvider>().userStudentId),
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  return Text(
-                                    snapshot.data.toString(),
-                                    style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-                                  );
-                                }
-                                return const Text('-', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15));
-                              },
-                            ),
-
-                          ]
-                      ),
-                    ),
-
-
-                    SizedBox(
-                      width: 100,
-                      child: Column(
-                          children:[
-                            const Text('WebCam', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: AppColor.Blue)),
-                            const SizedBox(height: 8),
-                            Container(
-                              height: 2.0,
-                              width: 70.0,
-                              color: AppColor.Blue,
-                            ),
-                            const SizedBox(height: 14),
-
-                            rentalStatus[2]
-                                ? const Icon(Icons.photo_camera, color: AppColor.Blue4, size: 65)
-                                : const Icon(Icons.close_rounded, color: AppColor.Grey1, size: 65),
-
-                            FutureBuilder<String>(
-                              future: getWebCamDuedate(context.read<UserProvider>().userStudentId),
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  return Text(
-                                    snapshot.data.toString(),
-                                    style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-                                  );
-                                }
-                                return const Text('-', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15));
-                              },
-                            ),
-
-                          ]
-                      ),
-                    ),
-                  ]
+                    );
+                  }
+                },
               ),
+
+
+
+
 
               const SizedBox(height: 25),
 
